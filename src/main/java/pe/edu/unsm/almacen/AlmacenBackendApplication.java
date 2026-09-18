6f10612e56c550c8b8c80bb5ac850fbf44f41442
Barc1710 <ba.rodascu@unsm.edu.pe>
2026-09-18 11:29:38 -0500

    Initial commit

---
+++ src/main/java/pe/edu/unsm/almacen/AlmacenBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unsm.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/unsm/almacen/AlmacenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unsm.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
